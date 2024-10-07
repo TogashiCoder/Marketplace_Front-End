bdda759a0463fb3d2e45d6cacffba065094c0120
Togahsi <silverproduction2023@gmail.com>
2024-10-07 15:06:20 +0100

    update home

--- src/app/home/navbar/navbar.component.ts
+++ src/app/home/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +11,51 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  showRegistrationPrompt: boolean = false;
+  registrationPromptMessage: string = '';
+  registrationPromptAction: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canPerformAction(): boolean {
+    return this.authService.isLoggedIn() && this.authService.getUserRole() === 'BUYER';
+  }
+
+  onFavoriteClick(): void {
+    if (!this.canPerformAction()) {
+      this.showRegistrationPrompt = true;
+      this.registrationPromptMessage = 'Please register to view  your wishlist.';
+      this.registrationPromptAction = 'addToWishlist';
+    } else {
+      // Logic to add favorite if the user is a buyer
+      this.router.navigate(['/shop/favorite']);
+
+    }
+  }
+
+  onBasketClick(): void {
+    if (!this.canPerformAction()) {
+      this.showRegistrationPrompt = true;
+      this.registrationPromptMessage = 'Please register to view your shopping cart.';
+      this.registrationPromptAction = 'viewCart';
+    } else {
+      // Logic to navigate to the shopping cart
+      this.router.navigate(['/shop/Shopping-cart']);
+    }
+  }
+
+  onRegister(): void {
+    this.showRegistrationPrompt = false;
+    this.router.navigate(['/login'], {
+      queryParams: {
+        returnUrl: this.router.url,
+        action: this.registrationPromptAction
+      }
+    });
+  }
+
+  onCloseRegistrationPrompt(): void {
+    this.showRegistrationPrompt = false;
+  }
 
 }
